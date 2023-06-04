e4fb72ddf35a6a7db84f09142cfe4456399876e2
hasan-coban <112540143+hasan-coban@users.noreply.github.com>
2023-06-04 23:15:06 +0300

    Create Unusual_five.js

---
+++ Unusual_five.js
@@ -0,0 +1,16 @@
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+*/
+//My S:
+function unusualFive() {
+let name="hasan";
+  return name.length;
+}
+//2nd :
+function unusualFive() {
+  return 'fucku'.length
+}
